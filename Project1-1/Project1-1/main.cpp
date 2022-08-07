75ab92c2fb78943a43250dd5ad0fd7e22626f298
Lyk <lyk9583@naver.com>
2022-08-07 14:51:13 +0900

    hello world 실습

---
+++ Project1-1/Project1-1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello world" << endl;
+    cout << "hello" << "toran" << endl << "why";
+    cout << "hi" << endl;
+    return 0;
+}
