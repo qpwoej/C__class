450b2d5fea24d98987e86d5f9567a2af723d24e1
Lyk <lyk9583@naver.com>
2022-08-07 15:53:33 +0900

    연산 실습

--- Project1-1/Project1-1/main.cpp
+++ Project1-1/Project1-1/main.cpp
@@ -3,13 +3,7 @@ using namespace std;
 
 int main(void)
 {
-    int currentHp = 100;
-    currentHp = 50;
-    cout << "현재 내 체력 :" << currentHp << endl;
-
-    int a = 10;
-    int b = 20;
-
-    cout << "a + b =" << a + b << endl;
+    int currentHp = 50;
+    cout << "현재 내 체력 :" << currentHp + 10 << endl;
     return 0;
 }
