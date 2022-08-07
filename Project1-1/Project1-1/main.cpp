d11419cd68d407d454699d61fefd5baaeb06ec40
Lyk <lyk9583@naver.com>
2022-08-07 15:27:25 +0900

    연산 실습

--- Project1-1/Project1-1/main.cpp
+++ Project1-1/Project1-1/main.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main(void)
 {
-    cout << "Hello world" << endl;
-    cout << "hello" << "toran" << endl << "why";
-    cout << "hi" << endl;
+    int currentHp = 100;
+    currentHp = 50;
+    cout << "현재 내 체력 :" << currentHp << endl;
+
+    int a = 10;
+    int b = 20;
+
+    cout << "a + b =" << a + b << endl;
     return 0;
 }
